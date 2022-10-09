abc87a834a758143b621904e052aa7efa3300244
Lukasz Pietraszek <pietraszek@hotmail.co.uk>
2022-10-09 21:01:14 +0200

    SwiftUI-we-split

---
+++ wesplitapp/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Lukasz Pietraszek on 09/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
